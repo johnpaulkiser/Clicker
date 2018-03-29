4ffa7bd23d450860de2314cbd1741107b3c53739
JP <johnpaulkiser@gmail.com>
2018-03-28 22:30:16 -0400

    worked on increasing inventory through +1 button

--- clicker.js
+++ clicker.js
@@ -1,42 +1,51 @@
-let wallet = 0;
+// *notes
+//	
+//
 
 class Event {
-	constructor(duration, price, amount_profits){
+	constructor(duration, price, amount_profits, id){
 	
-		this.duration = duration;
-		this.price = price;
-		this.amount_profits = amount_profits;
+		this.duration = duration; 					//how long user has to wait between clicks
+		this.price = price;							//currently tracks the price of upgrade. 
+		this.amount_profits = amount_profits;		//how much 1 click will profit
+		this.original_profits = amount_profits;		//
 		this.num = 1;
 		this.is_auto = false;
-
+		this.id = id;
+		document.getElementById(this.id).innerHTML = "" +this.id + " Cost: $" + this.price;
 	}
 
 	clicked(){
 		wallet += this.amount_profits;
 		document.getElementById("cash").innerHTML = "Current Balance: $" + wallet;
-		
 	}
-
+ 
 	auto(){
 		this.is_auto = true;
 	}
 
 	addOne(){
-		wallet -= price;
-		this.num++;
-		this.price += 0.25 * price;
-		this.amount_profits *= num;
+		if(wallet >= this.price) {
+			wallet -= this.price;
+			this.num++;
+			this.price += 0.25 * this.price;
+			this.amount_profits = this.original_profits * this.num;
+			document.getElementById(this.id).innerHTML = "(" + this.num +") "+this.id + "Cost: $" + this.price;
+			document.getElementById("cash").innerHTML = "Current Balance: $" + wallet;
 
+		}
 	}
 
 
 }
 
-var taco = new Event(0, 0, 1);
-var taquero = new Event(3, 40, 50);
-var taco_stand = new Event(5, 200, 300);
-var taco_truck = new Event(10, 1000, 1250);
-var restaurant = new Event(25, 20000, 4000);
+let wallet = 0.00;
+
+var taco = new Event(0, 10, 1, "Taco");
+var taquero = new Event(3, 40, 50, "Taquero");
+var taco_stand = new Event(5, 200, 300, "Taco Stand");
+var taco_truck = new Event(10, 1000, 1250, "Taco Truck");
+var restaurant = new Event(25, 20000, 4000, "Restaurant");
 
 
 
