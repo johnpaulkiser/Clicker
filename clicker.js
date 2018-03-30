5a1babce5a746a301a01053bf8cd2e9ec323219e
JP <johnpaulkiser@gmail.com>
2018-03-30 12:36:58 -0400

    updated addOne cost

--- clicker.js
+++ clicker.js
@@ -1,9 +1,9 @@
 // *notes
-//	
+//	Needs timers that disable buttons   -->auto
 //
 
 class Event {
-	constructor(duration, price, amount_profits, id){
+	constructor(duration, price, amount_profits, event_id, addOne_id){
 	
 		this.duration = duration; 					//how long user has to wait between clicks
 		this.price = price;							//currently tracks the price of upgrade. 
@@ -11,12 +11,15 @@ class Event {
 		this.original_profits = amount_profits;		//
 		this.num = 1;
 		this.is_auto = false;
-		this.id = id;
-		document.getElementById(this.id).innerHTML = "" +this.id + " Cost: $" + this.price;
+		this.event_id = event_id;
+		this.addOne_id = addOne_id;
+		document.getElementById(this.addOne_id).innerHTML = "Cost: $" + this.price;
+		document.getElementById(this.event_id).innerHTML = this.event_id;
 	}
 
 	clicked(){
 		wallet += this.amount_profits;
+		wallet = parseInt(wallet.toFixed(2));
 		document.getElementById("cash").innerHTML = "Current Balance: $" + wallet;
 	}
  
@@ -29,8 +32,10 @@ class Event {
 			wallet -= this.price;
 			this.num++;
 			this.price += 0.25 * this.price;
+			this.price = parseInt(this.price.toFixed(2));
 			this.amount_profits = this.original_profits * this.num;
-			document.getElementById(this.id).innerHTML = "(" + this.num +") "+this.id + "Cost: $" + this.price;
+			document.getElementById(this.addOne_id).innerHTML = "Cost: $" + this.price;
+			document.getElementById(this.event_id).innerHTML = "(" + this.num +") " + this.event_id;
 			document.getElementById("cash").innerHTML = "Current Balance: $" + wallet;
 
 		}
@@ -41,11 +46,11 @@ class Event {
 
 let wallet = 0.00;
 
-var taco = new Event(0, 10, 1, "Taco");
-var taquero = new Event(3, 40, 50, "Taquero");
-var taco_stand = new Event(5, 200, 300, "Taco Stand");
-var taco_truck = new Event(10, 1000, 1250, "Taco Truck");
-var restaurant = new Event(25, 20000, 4000, "Restaurant");
+var taco = new Event(0, 10, 1, "Taco", "taco1");
+var taquero = new Event(3, 40, 50, "Taquero", "taco2");
+var taco_stand = new Event(5, 200, 300, "Taco Stand", "taco3");
+var taco_truck = new Event(10, 1000, 1250, "Taco Truck", "taco4");
+var restaurant = new Event(25, 20000, 4000, "Restaurant", "taco5");
 
 
 
