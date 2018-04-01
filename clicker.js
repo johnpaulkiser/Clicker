07102ad7f8237280884e7f32c61cc157f595ad0c
JP <johnpaulkiser@gmail.com>
2018-04-01 19:04:01 -0400

    everything works but the automate feature

--- clicker.js
+++ clicker.js
@@ -3,7 +3,7 @@
 //
 
 class Event {
-	constructor(duration, price, amount_profits, event_id, addOne_id){
+	constructor(duration, price, amount_profits, event_id, addOne_id, disabled){  //duration in milliseconds
 	
 		this.duration = duration; 					//how long user has to wait between clicks
 		this.price = price;							//currently tracks the price of upgrade. 
@@ -15,16 +15,36 @@ class Event {
 		this.addOne_id = addOne_id;
 		document.getElementById(this.addOne_id).innerHTML = "Cost: $" + this.price;
 		document.getElementById(this.event_id).innerHTML = this.event_id;
+		this.disabled = disabled;
+		if (disabled){
+			document.getElementById(event_id).innerHTML = "Unlock "+event_id+" for $"+price;
+			document.getElementById(addOne_id).disabled = true; 
+		} 
 	}
 
 	clicked(){
-		wallet += this.amount_profits;
-		wallet = parseInt(wallet.toFixed(2));
-		document.getElementById("cash").innerHTML = "Current Balance: $" + wallet;
+		if (this.disabled == false) {
+			this.timer();
+		} else if (wallet >= this.price) {
+			this.disabled = false;
+			wallet -= this.price;
+			document.getElementById("cash").innerHTML = "Current Balance: $" + wallet.toFixed(2);
+			document.getElementById(this.addOne_id).disabled = false; 
+			document.getElementById(this.event_id).innerHTML = this.event_id;
+		}
+
 	}
- 
-	auto(){
-		this.is_auto = true;
+
+	timer(){ //disables an event for duration in milli
+		document.getElementById(this.event_id).disabled = true;
+		setTimeout(enableButton, this.duration, this.amount_profits, this.event_id);
+	}
+
+
+	auto(){ 
+		this.is_auto = true; 
+		                                      
+
 	}
 
 	addOne(){
@@ -32,25 +52,42 @@ class Event {
 			wallet -= this.price;
 			this.num++;
 			this.price += 0.25 * this.price;
-			this.price = parseInt(this.price.toFixed(2));
+			this.price = parseFloat(this.price.toFixed(2));
 			this.amount_profits = this.original_profits * this.num;
 			document.getElementById(this.addOne_id).innerHTML = "Cost: $" + this.price;
 			document.getElementById(this.event_id).innerHTML = "(" + this.num +") " + this.event_id;
-			document.getElementById("cash").innerHTML = "Current Balance: $" + wallet;
+			document.getElementById("cash").innerHTML = "Current Balance: $" + wallet.toFixed(2);
 
 		}
 	}
 
-
 }
+
+function enableButton(amount, id){	//enables button and pays out to wallet once setTimeout calls
+	document.getElementById(id).removeAttribute('disabled');
+	wallet += amount;
+	wallet = parseFloat(wallet.toFixed(2));
+	document.getElementById("cash").innerHTML = "Current Balance: $" + wallet.toFixed(2);
+	
+}
+
+function autod(duration, click_func){
+	if (duration === 0){
+		duration = 1000;
+	}
+	setInterval(click_func, duration);
+}
+
+
+
 
 let wallet = 0.00;
 
-var taco = new Event(0, 10, 1, "Taco", "taco1");
-var taquero = new Event(3, 40, 50, "Taquero", "taco2");
-var taco_stand = new Event(5, 200, 300, "Taco Stand", "taco3");
-var taco_truck = new Event(10, 1000, 1250, "Taco Truck", "taco4");
-var restaurant = new Event(25, 20000, 4000, "Restaurant", "taco5");
+var taco = new Event(0, 10, 1, "Taco", "taco1", false);
+var taquero = new Event(3000, 40, 50, "Taquero", "taco2", true);
+var taco_stand = new Event(5000, 200, 300, "Taco Stand", "taco3", true);
+var taco_truck = new Event(10000, 1000, 1250, "Taco Truck", "taco4", true);
+var restaurant = new Event(25000, 20000, 4000, "Restaurant", "taco5", true);
 
 
 
